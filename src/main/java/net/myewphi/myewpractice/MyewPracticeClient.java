d2cdf8e0f7c63255c65932b5d7f0b5aaebfdd45f
Myewphi <54916148+Myewphi@users.noreply.github.com>
2024-04-06 20:13:15 -0700

    Initial commit

---
+++ src/main/java/net/myewphi/myewpractice/MyewPracticeClient.java
@@ -0,0 +1,10 @@
+package net.myewphi.myewpractice;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyewPracticeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/myewphi/myewpractice/MyewPracticeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.myewphi.myewpractice;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MyewPracticeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
